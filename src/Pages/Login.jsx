1b355467bd4464c2710558180481f14eea0cdd40
jiin9999 <cji3604@gmail.com>
2023-06-20 11:38:08 +0900

    Feat: InputBox 완성 & Login Page 완성

--- src/Pages/Login.jsx
+++ src/Pages/Login.jsx
@@ -1,9 +1,11 @@
 import WelcomeLogin from "../Components/Login/WelcomeLogin";
+import InputBox from "../Components/Login/InputBox";
 
 const Login = () => {
   return (
     <div className="page-container">
       <WelcomeLogin />
+      <InputBox />
     </div>
   );
 };

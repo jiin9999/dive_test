615162be0e2fee4019fd1c4b45b26f7a2c1521d3
jiin9999 <cji3604@gmail.com>
2023-06-20 10:47:31 +0900

    Style: Login 페이지 Welcome 완성

--- src/Pages/Login.jsx
+++ src/Pages/Login.jsx
@@ -1,5 +1,11 @@
+import WelcomeLogin from "../Components/Login/WelcomeLogin";
+
 const Login = () => {
-  return <div className="page-container"></div>;
+  return (
+    <div className="page-container">
+      <WelcomeLogin />
+    </div>
+  );
 };
 
 export default Login;

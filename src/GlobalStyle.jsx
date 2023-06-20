1e95ab158666f9969d7e14f46bed691103c41780
jiin9999 <cji3604@gmail.com>
2023-06-20 10:08:29 +0900

    Style: styled-reset 적용, 글로벌 스타일 변경

--- src/GlobalStyle.jsx
+++ src/GlobalStyle.jsx
@@ -1,14 +1,19 @@
 import { createGlobalStyle } from "styled-components";
+import reset from "styled-reset";
 
 const GlobalStyle = createGlobalStyle`
+  ${reset}
+  
   * {
     box-sizing: border-box;
-    margin: 0;
-    font-family: "Pretendard GOV Variable", "Pretendard GOV", -apple-system, BlinkMacSystemFont, 
+  }  
+
+  body { 
+    font-family: "Pretendard GOV Variable", -apple-system, BlinkMacSystemFont, 
     system-ui, Roboto, "Helvetica Neue", "Segoe UI", "Apple SD Gothic Neo", "Noto Sans KR", 
     "Malgun Gothic", "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol", sans-serif;
     font-weight: 500;
-  }  
+  }
 
   // 헤더의 컨테이너도 겸하고 있습니다.
   .page-container {

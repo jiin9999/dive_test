74ed4e5661fe8787d7893dde7b38639f8609080c
jiin9999 <cji3604@gmail.com>
2023-06-20 14:36:36 +0900

    Feat: Chats 페이지 ChatsBox 컴포넌트 완성
    MockData를 받아 컴포넌트 내부에서 map으로 호출하도록 구현
    ChatContainer에 grid 레이아웃 사용

--- src/Pages/Chats.jsx
+++ src/Pages/Chats.jsx
@@ -1,9 +1,13 @@
 import ChatsHeader from "../Components/Chats/ChatsHeader";
+import ChatsBox from "../Components/Chats/ChatsBox";
 
 const Chats = () => {
   return (
     <div className="page-container">
       <ChatsHeader />
+      <main>
+        <ChatsBox />
+      </main>
     </div>
   );
 };
